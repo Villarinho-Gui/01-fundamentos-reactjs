0637b473f3ffd66d2f211588d6ffbeb12afea292
Villa <Guivillarinho15@gmail.com>
2022-12-19 11:28:15 -0300

    style: Estilização da SideBar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,16 @@ import styles from './Sidebar.module.css'
 export function Sidebar(){
   return (
     <>
-      <aside className={styles.sidebar}>opa</aside>
+      <aside className={styles.sidebar}>
+        <figure>
+          <img 
+          src="https://github.com/Villarinho-Gui.png" 
+          alt="Foto de perfil" 
+          />
+        </figure>
+        <h2>Guilherme Villarinho</h2>
+        <p>Front-End Developer</p>
+      </aside>
     </>
   )
 }
