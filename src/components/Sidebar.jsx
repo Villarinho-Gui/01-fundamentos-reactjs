381042b3d7828b852d69cf9c2c090c89c6a4fd9b
guilhermevillarinho <guivillarinho15@gmail.com>
2022-12-22 11:57:26 -0300

    style: adding some elements in code

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,27 @@
-import styles from './Sidebar.module.css'
+import styles from "./Sidebar.module.css";
 
-export function Sidebar(){
+export function Sidebar() {
   return (
     <>
       <aside className={styles.sidebar}>
-        <figure>
-          <img 
-          src="https://github.com/Villarinho-Gui.png" 
-          alt="Foto de perfil" 
-          />
-        </figure>
-        <h2>Guilherme Villarinho</h2>
-        <p>Front-End Developer</p>
+        <img
+          className={styles.cover}
+          src="https://images.pexels.com/photos/574071/pexels-photo-574071.jpeg?auto=compress&cs=tinysrgb&w=500&h=750&dpr=1"
+        />
+        <div className={styles.profile}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/Villarinho-Gui.png"
+            alt="Foto de perfil"
+          /> 
+
+          <strong>Guilherme Villarinho</strong>
+          <span>Web Developer</span>
+        </div>
+        <footer>
+          <a href="#">Editar seu Perfil</a>
+        </footer>
       </aside>
     </>
-  )
+  );
 }
